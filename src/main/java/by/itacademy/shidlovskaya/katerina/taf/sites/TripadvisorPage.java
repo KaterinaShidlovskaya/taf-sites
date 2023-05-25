59c778e7f760257f79c0e4c96a87500510a838e4
Katerina Shidlovskaya <katerinashidlovskaya2711@gmail.com>
2023-05-25 13:36:25 +0300

    add modifications to TripadvisorPage and TripadvisorTest classes

--- src/main/java/by/itacademy/shidlovskaya/katerina/taf/sites/TripadvisorPage.java
+++ src/main/java/by/itacademy/shidlovskaya/katerina/taf/sites/TripadvisorPage.java
@@ -1,9 +1,53 @@
 package by.itacademy.shidlovskaya.katerina.taf.sites;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class TripadvisorPage {
-    String buttonLoginXpath ="//*[@id= 'lithium-root']/header/div/nav/div/div[2]/a[3]";
-    String buttonContinueWithEmailXpath ="//*[@id='ssoButtons']/button";
-    String inputEmailAddressXpath = "//*[@id='regSignIn.email']";
-    String inputPasswordXpath ="//*[@id='regSignIn.password']";
-    String buttonSingInXpath = "//*[@id='regSignIn']/div[4]/button[1]";
+    private ChromeDriver driver;
+    private String buttonLoginXpath = "//*[@id= 'lithium-root']/header/div/nav/div/div[2]/a[3]";
+    private String buttonContinueWithEmailXpath = "//*[@id='ssoButtons']/button";
+    private String inputEmailAddressXpath = "//*[@id='regSignIn.email']";
+    private String inputPasswordXpath = "//*[@id='regSignIn.password']";
+    private String buttonSingInXpath = "//*[@id='regSignIn']/div[4]/button[1]";
+    private String baseURL = "https://www.tripadvisor.com/";
+
+    public TripadvisorPage(ChromeDriver newDriver) {
+        driver = newDriver;
+    }
+
+    public void openBaseURL() {
+        driver.get(baseURL);
+    }
+
+    public void clickButtonRegister() {
+        WebElement buttonRegister = driver.findElement(By.xpath(buttonLoginXpath));
+        buttonRegister.click();
+    }
+
+    public void clickButtonContinueWithEmail() {
+        WebElement buttonContinueWithEmail = (new WebDriverWait(driver, Duration.ofSeconds(20))).
+                until(ExpectedConditions.elementToBeClickable(By.xpath(buttonContinueWithEmailXpath)));
+        buttonContinueWithEmail.click();
+    }
+
+    public void clickButtonSingIn(){
+        WebElement buttonSingIn = driver.findElement(By.xpath(buttonSingInXpath));
+        buttonSingIn.click();
+    }
+
+    public void sendKeysInputEmailAddress(String email){
+        WebElement inputEmail = driver.findElement(By.xpath(inputEmailAddressXpath));
+        inputEmail.sendKeys(email);
+    }
+
+    public void sendKeysInputPassword(String password){
+        WebElement inputPassword = driver.findElement(By.xpath(inputPasswordXpath));
+        inputPassword.sendKeys(password);
+    }
 }
--- src/test/java/by/itacademy/shidlovskaya/katerina/taf/sites/TripadvisorTest.java
+++ src/test/java/by/itacademy/shidlovskaya/katerina/taf/sites/TripadvisorTest.java
@@ -3,12 +3,7 @@ package by.itacademy.shidlovskaya.katerina.taf.sites;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
 public class TripadvisorTest {
     ChromeDriver driver;
@@ -17,48 +12,35 @@ public class TripadvisorTest {
     @BeforeEach
     public void warmUp() {
         driver = new ChromeDriver();
-        tripadvisorPage = new TripadvisorPage();
-        driver.get("https://www.tripadvisor.com/");
-        WebElement buttonRegister = driver.findElement(By.xpath(tripadvisorPage.buttonLoginXpath));
-        buttonRegister.click();
-
-        WebElement buttonContinueWithEmail = (new WebDriverWait(driver, Duration.ofSeconds(20))).
-                until(ExpectedConditions.elementToBeClickable(By.xpath(tripadvisorPage.buttonContinueWithEmailXpath)));
-        buttonContinueWithEmail.click();
-
+        tripadvisorPage = new TripadvisorPage(driver);
+        driver.manage().window().maximize();
+        tripadvisorPage.openBaseURL();
+        tripadvisorPage.clickButtonRegister();
+        tripadvisorPage.clickButtonContinueWithEmail();
     }
 
     @Test
     public void testRegisterWithEmptyEmailAndPassword() {
-     WebElement buttonSingIn = driver.findElement(By.xpath(tripadvisorPage.buttonSingInXpath));
-     buttonSingIn.click();
+        tripadvisorPage.clickButtonSingIn();
     }
-    @Test
-    public void testLoginWithIncorrectEmail(){
-        WebElement inputEmail =driver.findElement(By.xpath(tripadvisorPage.inputEmailAddressXpath));
-        inputEmail.sendKeys("email");
 
-        WebElement buttonSingIn = driver.findElement(By.xpath(tripadvisorPage.buttonSingInXpath));
-        buttonSingIn.click();
+    @Test
+    public void testLoginWithIncorrectEmail() {
+        tripadvisorPage.sendKeysInputEmailAddress("email");
+        tripadvisorPage.clickButtonSingIn();
     }
-    @Test
-    public void testLoginWithCorrectEmailAndEmptyPassword(){
-        WebElement inputEmail =driver.findElement(By.xpath(tripadvisorPage.inputEmailAddressXpath));
-        inputEmail.sendKeys("test@mail.com");
 
-        WebElement buttonSingIn = driver.findElement(By.xpath(tripadvisorPage.buttonSingInXpath));
-        buttonSingIn.click();
+    @Test
+    public void testLoginWithCorrectEmailAndEmptyPassword() {
+        tripadvisorPage.sendKeysInputEmailAddress(Util.generateEmail());
+        tripadvisorPage.clickButtonSingIn();
     }
+
     @Test
-    public void testLoginWithCorrectEmailAndPassword(){
-        WebElement inputEmail =driver.findElement(By.xpath(tripadvisorPage.inputEmailAddressXpath));
-        inputEmail.sendKeys("test@mail.com");
-
-        WebElement inputPassword = driver.findElement(By.xpath(tripadvisorPage.inputPasswordXpath));
-        inputPassword.sendKeys("12345");
-
-        WebElement buttonSingIn = driver.findElement(By.xpath(tripadvisorPage.buttonSingInXpath));
-        buttonSingIn.click();
+    public void testLoginWithCorrectEmailAndPassword() {
+        tripadvisorPage.sendKeysInputEmailAddress(Util.generateEmail());
+        tripadvisorPage.sendKeysInputPassword(Util.generatePassword());
+        tripadvisorPage.clickButtonSingIn();
     }
 
     @AfterEach
