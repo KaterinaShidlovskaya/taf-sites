ad1ee6a5594ebee848bcbf5764ba6e0de8b5217a
Katerina Shidlovskaya <katerinashidlovskaya2711@gmail.com>
2023-05-28 12:54:03 +0300

    add DominosStep class

---
+++ src/main/java/by/itacademy/shidlovskaya/katerina/taf/sites/DominosStep.java
@@ -0,0 +1,29 @@
+package by.itacademy.shidlovskaya.katerina.taf.sites;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DominosStep {
+    DominosPage page;
+
+    public DominosStep(ChromeDriver driver) {
+        page = new DominosPage(driver);
+    }
+    public void openURLAndClickButtonLogin(){
+        page.openBaseURL();
+        page.clickButtonPopUpClose();
+        page.clickButtonLogin();
+    }
+    public void fillLoginFormWithInvalidEmailAndAnyPassword(String invalidEmail, String password) {
+        page.sendKeysInputEmailAddress(invalidEmail);
+        page.sendKeysInputPassword(password);
+        page.clickButtonSingIn();
+    }
+    public void fillLoginFormWithAnyEmailAndPassword(String email, String password) {
+        page.sendKeysInputEmailAddress(email);
+        page.sendKeysInputPassword(password);
+        page.clickButtonSingIn();
+    }
+    public void finishTheWork(){
+        page.quitDriver();
+    }
+}
